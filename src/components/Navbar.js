1ec762b67a0bda2aacf0932e17df781de6adc888
parth-u <menonparth765@gmail.com>
2023-10-14 20:39:29 +0530

    done navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Navbar.css";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import prof from "./prof.jpg";
+import logo from "./pimslogo.png";
+import { useNavigate } from "react-router-dom";
+function Navbar() {
+  const navigate = useNavigate();
+  return (
+    <div className="top">
+      <h3>Personal Information Managment System</h3>
+      <img className="logo" src={logo} atl="" />
+
+      <Stack className="topsign" spacing={2} direction="row">
+        <Avatar className="topavatar" src={prof} />
+        <button
+          onClick={() => navigate("/home")}
+          className="topbutton"
+          size="small"
+          variant="contained"
+        >
+          SignUp
+        </button>
+      </Stack>
+    </div>
+  );
+}
+
+export default Navbar;
