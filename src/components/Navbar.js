e81223cf80ebfd1f0924720082ce66e949440d8d
parth-u <menonparth765@gmail.com>
2023-10-15 20:59:33 +0530

    did some finishing work

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,14 +4,14 @@ import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
 import prof from "./prof.jpg";
-import logo from "./pimslogo.png";
+import logo1 from "./pimslogo.png";
 import { useNavigate } from "react-router-dom";
 function Navbar() {
   const navigate = useNavigate();
   return (
     <div className="top">
       <h3>Personal Information Managment System</h3>
-      <img className="logo" src={logo} atl="" />
+      <img className="logo" src={logo1} atl="" />
 
       <Stack className="topsign" spacing={2} direction="row">
         <Avatar className="topavatar" src={prof} />
